9c3a99ed358b734c72d5d750cb5c106b5fd79ff0
asmaaMahmoudF <asmaafahmy932@gmail.com>
2023-12-02 02:20:56 +0200

    task_11

--- 0x01-python-if_else_loops_functions/9-main.py
+++ 0x01-python-if_else_loops_functions/9-main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-add = __import__('10-add').add
+pow = __import__('11-pow').pow
 
-print(add(1, 2))
-print(add(98, 0))
-print(add(100, -2))
+print(pow(2, 2))
+print(pow(98, 2))
+print(pow(98, 0))
+print(pow(100, -2))
+print(pow(-4, 5))
 
 #  return print("{:d}".format(abs(number) % 10), end="")
