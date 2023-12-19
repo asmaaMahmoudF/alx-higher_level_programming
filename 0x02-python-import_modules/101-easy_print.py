21c0af5362c5aa16ee2c1f27a77a6a8640ce222a
asmaaMahmoudF <asmaafahmy932@gmail.com>
2023-12-19 04:33:50 +0200

    task_7

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-try: raise Exception
-except Exception as e: e.__context__.__class__.__base__.__subclasses__()[59]()._Printer__setup()._stream.write('#pythoniscool\n')
+print('#pythoniscool\n')
