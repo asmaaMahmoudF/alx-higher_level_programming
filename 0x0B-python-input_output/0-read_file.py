05fa09efb512a17f968d134347fc92968fc702dd
asmaaMahmoudF <asmaafahmy932@gmail.com>
2024-01-10 03:34:13 +0200

    Task_0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """read my_file_0.txt with utf-8"""
     with open('my_file_0', encoding='utf-8') as f:
         data = f.read()
-        print(data, end="")
+        print(data)
