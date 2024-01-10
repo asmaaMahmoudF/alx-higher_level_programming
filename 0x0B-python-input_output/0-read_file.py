8f816fdbf52031f1d04b7b2a5a068af7639134c8
asmaaMahmoudF <asmaafahmy932@gmail.com>
2024-01-10 03:29:31 +0200

    Task_0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """defining read_file function"""
 
+
 def read_file(filename=""):
     """read my_file_0.txt with utf-8"""
     with open('my_file_0.txt', 'r', encoding='utf-8') as f:
