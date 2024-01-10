b7a547fa0dd2085b03503bddc6d420da0c53519b
asmaaMahmoudF <asmaafahmy932@gmail.com>
2024-01-10 04:27:30 +0200

    Task_1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 
 def write_file(filename="", text=""):
      """write 1-write_file.txt with utf-8"""
-     with open(filename, 'w', encoding='utf-8') as f:
+     with open(filename, "w", encoding='utf-8') as f:
         return(f.write(text))
