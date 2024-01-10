1003f78b7f8acc56bcd4213a5766b7c48280e261
asmaaMahmoudF <asmaafahmy932@gmail.com>
2024-01-10 02:47:00 +0200

    Task_0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    with open('my_file_0.txt','r') as file:
+    with open('my_file_0.txt','r',encoding='utf-8') as file:
         data = file.read()
     print(data)
