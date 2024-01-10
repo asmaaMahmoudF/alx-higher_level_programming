bd0811cc9e1faad09d69b04807adc4e77bfd2e89
asmaaMahmoudF <asmaafahmy932@gmail.com>
2024-01-10 04:48:21 +0200

    Task_3

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""defining to_json_string function"""
+import json
+
+
+def from_json_string(my_str):
+    """convert JSON string into python object"""
+    return json.loads(my_str)
