0946c9a59a3172d6befe42585e80470ae20cab6b
asmaaMahmoudF <asmaafahmy932@gmail.com>
2024-01-11 03:45:10 +0200

    task_7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,10 @@
 #!/usr/bin/python3
 """defining save_to_json_file function"""
-import json
 import sys
 
 
-def save_to_json_file(my_obj, filename):
-    """Save an object to json file"""
-    with open(filename, 'w', encoding='utf-8') as f:
-        json.dump(my_obj, f)
-
-
-def load_from_json_file(filename):
-    """load_from_json_file"""
-    with open(filename, 'r') as f:
-        data = json.load(f)
-        return data
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 arglist = list(sys.argv[1:])
