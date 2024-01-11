576391a43d6fa83d7656b6a6f9e066e369cb9cc9
asmaaMahmoudF <asmaafahmy932@gmail.com>
2024-01-11 03:51:36 +0200

    task_7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-"""defining save_to_json_file function"""
+"""add item"""
 import sys
 
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
-arglist = list(sys.argv[1:])
-
+filename = "add_item.json"
 try:
-    old_data = load_from_json_file('add_item.json')
+    old_data = load_from_json_file(filename)
 except Exception:
     old_data = []
 
-old_data.extend(arglist)
-save_to_json_file(old_data, "add_item.json")
+old_data.extend(sys.argv[1:])
+save_to_json_file(old_data, filename)
