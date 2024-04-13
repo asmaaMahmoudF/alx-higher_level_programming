65cd09fdfc37e378d7b65752a9de76bb5d8c3bff
asmaaMahmoudF <asmaafahmy932@gmail.com>
2024-04-14 01:26:40 +0200

    task_2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -45,24 +45,30 @@ def matrix_divided(matrix, div):
     Returns:
         list : list of lists representing divided matrix.
     """
+    """ Divide a matrix by a number div """
+    list_error = "matrix must be a matrix (list of lists) of integers/floats"
+    len_error = "Each row of the matrix must have the same size"
+    div_int_error = "div must be a number"
+    div_zero_error = "division by zero"
+    new_matrix = []
+    new_list = []
+    if not matrix:
+        raise TypeError(list_error)
     if type(div) is not int and type(div) is not float:
-        raise TypeError("div must be a number")
+        raise TypeError(div_int_error)
     if div is 0:
-        raise ZeroDivisionError("division by zero")
-    for row in matrix:
-        if not isinstance(row, list) or len(row) == 0:
-            raise TypeError("matrix must be a matrix (list of lists) of \
-                             integers/floats")
-        if len(row) != len(matrix[0]):
-            raise TypeError("Each row of the matrix must have the same size")
-        for x in row:
-            if not isinstance(x, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of \
-                                 integers/floats")
-    return [[round(x / div, 2) for x in row] for row in matrix]
-
-
-if __name__ == "__main__":
-
-    import doctest
-    doctest.testfile("tests/2-matrix_divided.txt")
+        raise ZeroDivisionError(div_zero_error)
+    longitud = len(matrix[0])
+    for lista in matrix:
+        if type(lista) is not list:
+            raise TypeError(list_error)
+        if len(lista) != longitud:
+            raise TypeError(len_error)
+        for item in lista:
+            if type(item) is not int and type(item) is not float:
+                raise TypeError(list_error)
+            num = item / div
+            new_list.append(round(num, 2))
+        new_matrix.append(new_list)
+        new_list = []
+    return new_matrix
