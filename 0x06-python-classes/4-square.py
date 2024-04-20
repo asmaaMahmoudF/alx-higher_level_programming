887eb8033216613b10c4b42185d734ed458f8507
asmaaMahmoudF <asmaafahmy932@gmail.com>
2024-04-20 20:02:35 +0200

    task_44

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -15,6 +15,7 @@ class Square:
         self.__size = value
         if value < 0:
             raise ValueError("size must be >= 0")
+
     def __init__(self, size=0):
         self.__size = size
 
