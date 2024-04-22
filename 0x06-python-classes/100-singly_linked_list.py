b52a5e0c5ba0ccad897dd272804a8df8e4c1eea1
asmaaMahmoudF <asmaafahmy932@gmail.com>
2024-04-22 02:45:20 +0200

    task_99

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -5,6 +5,10 @@
 class Node:
     """class Node """
 
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
     @property
     def data(self):
         return self._data
@@ -17,31 +21,28 @@ class Node:
 
     @property
     def next_node(self):
-        return self.next_node
+        return self._next_node
 
     @next_node.setter
     def next_node(self, value):
         if value is not None and not isinstance(value, Node):
             raise TypeError("next_node must be a Node object")
-        self.next_node = value
-
-    def __init__(self, data, next_node=None):
-        self.__data = data
-        self.__next_node = next_node
-
+        self._next_node = value
 
 class SinglyLinkedList:
     """class SinglyLinkedList"""
 
     def __init__(self):
-        self._head = None
+        self.head = None
 
     def sorted_insert(self, value):
         new_node = Node(value)
-        if not self._head:
+        if not self._head or self.head.data >= value:
+            new_node.next_node = self.head
             self._head = new_node
-            return
-        current = self._head
-        while current.next:
-            current = current.next
-        current.next = new_node
+        else:
+            current = self._head
+            while current.next and current.next.data < value:
+                current = current.next
+            new_node.next = current.next
+            current.next = new_node
