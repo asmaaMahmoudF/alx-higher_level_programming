839141edef84bfa3cfba99e68162c46894504aae
asmaaMahmoudF <asmaafahmy932@gmail.com>
2024-04-26 17:35:34 +0300

    task_1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,7 +6,7 @@ Defining a class Rectangle
 
 class Rectangle:
     """class Rectangle that defines a rectangle"""
-    def __init__(self, width=0, height=0):
+    def _init_(self, width=0, height=0):
         """initializes the rectangle
         Args:
             width (int): The width of the new rectangle.
@@ -24,40 +24,30 @@ class Rectangle:
             self.height = height
             self.width = width
 
-
     @property
     def width(self):
         """getter for the private instance attribute width"""
         return self.__width
 
-
     @width.setter
     def width(self, value):
         """setter for the private instance with width"""
         if type(value) is not int:
-            try:
-                value = int(value)
-            except ValueError:
-                raise TypeError("width must be an integer")
-        if value < 0:
+            raise TypeError("width must be an integer")
+        elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-
 
     @property
     def height(self):
         """getter for the private instance attribute height"""
         return self.__height
 
-
     @height.setter
     def height(self, value):
         """setter for the private instance with height"""
         if type(value) is not int:
-            try:
-                value = int(value)
-            except ValueError:
-                raise TypeError("height must be an integer")
-        if value < 0:
+            raise TypeError("height must be an integer")
+        elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
