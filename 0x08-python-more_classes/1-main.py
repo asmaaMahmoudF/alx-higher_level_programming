1dff6ff5a02a33cdb7af66738d32a5da4f0080ac
asmaaMahmoudF <asmaafahmy932@gmail.com>
2024-04-27 03:54:58 +0300

    task_1

--- 0x08-python-more_classes/1-main.py
+++ 0x08-python-more_classes/1-main.py
@@ -3,12 +3,12 @@ Rectangle = __import__('1-rectangle').Rectangle
 
 try:
     my_rectangle = Rectangle(2, 3)
-    my_rectangle.height = -4
+    my_rectangle.width = -4
 except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
 
 try:
     my_rectangle = Rectangle(2, 3)
-    my_rectangle.height = "4"
+    my_rectangle.width = "4"
 except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
