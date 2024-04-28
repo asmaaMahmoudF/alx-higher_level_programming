4513057997221f0e0fab0a21224540a78a9a9b9c
asmaaMahmoudF <asmaafahmy932@gmail.com>
2024-04-28 04:36:10 +0300

    task_5

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -78,5 +78,5 @@ class Rectangle:
 
     def __del__(self):
         """delet object/instance"""
-        print("Bye rectangle..")
+        print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
