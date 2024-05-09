595217d27fa4f52e5af34113af35fc93fc15a7e8
asmaaMahmoudF <asmaafahmy932@gmail.com>
2024-05-09 20:37:26 +0300

    task_0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -5,5 +5,5 @@ and methods of an object
 
 
 def lookup(obj):
-	"""hat returns the list of available attributes and methods of an object"""
-	print(dir(obj))
+    """hat returns the list of available attributes and methods of an object"""
+    print(dir(obj))
