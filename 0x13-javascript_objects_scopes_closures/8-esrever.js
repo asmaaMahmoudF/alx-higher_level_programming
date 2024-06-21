6cf1d8b99448fd58992d2ae0f8a68c3abba32c4d
asmaaMahmoudF <asmaafahmy932@gmail.com>
2024-06-21 04:04:25 +0300

    jsafterfixing

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,10 +2,10 @@
 // function that returns the reversed version of a list withouth reverse
 
 exports.esrever = function (list) {
-	const reversedList = [];
-	const last = list.length - 1;
-	for (let i = last; i >= 0; i--) {
+  const reversedList = [];
+  const last = list.length - 1;
+  for (let i = last; i >= 0; i--) {
 	  reversedList.push(list[i]);
-	}
-	return (reversedList);
-  };
+  }
+  return (reversedList);
+};
