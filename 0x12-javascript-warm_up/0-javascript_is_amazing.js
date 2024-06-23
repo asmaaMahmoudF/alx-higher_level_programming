a0224f1da7864139f69a6a73f0dcfadf96fcafc5
asmaaMahmoudF <asmaafahmy932@gmail.com>
2024-06-23 21:49:57 +0300

    js_0

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// #!/Users/asmaamahmoud/.nvm/versions/node/v20.15.0/bin/node
+//#!/Users/asmaamahmoud/.nvm/versions/node/v20.15.0/bin/node
 
 const myVar = 'JavaScript is amazing';
 console.log(myVar);
