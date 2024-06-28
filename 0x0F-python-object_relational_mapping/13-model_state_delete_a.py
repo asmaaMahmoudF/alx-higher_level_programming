1e40651d0600ff449f84ad5ebd45c9ac79895904
asmaaMahmoudF <asmaafahmy932@gmail.com>
2024-06-29 02:20:28 +0300

    up

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-deltes all State objects with letter a
+deletes all State objects with letter a
 """
 if __name__ == '__main__':
     from sqlalchemy import create_engine, MetaData
